a5c73e25bfbe7ed142ffd613bb7b18ac16293b10
Diego.AsusLaptop <diegoivanperdomoleon@gmail.com>
2022-04-04 03:45:28 +0930

    I need to have to different file for Client and Server

--- spellchecker/spell_check_client_server.py
+++ spellchecker/spell_check_client_server.py
@@ -8,62 +8,46 @@ Created on Fri Mar 18 17:36:29 2022
 #-Import necessary libraries 
 
 #--import socket programming library
-import socket
+import socket, threading, textblob
 
-# import thread module
-from _thread import *
-import threading
+def spellChecker(msg):
+    arrWords = msg.split()
+    for word in arrWords:
+        word.lower()
+        print("Word no spelling correction: ", word)
+        # w = textblob.TextBlob(word)
+        w = textblob.TextBlob(word)
+        w2 = w.correct()
+        if w != w2:
+            print('Original Word {} - Suggested Word: {}'.format(w, w2))
 
-print_lock = threading.Lock()
-
-#Thread function
-def threaded(c):
-    while True:
-        
-        #Data received from client
-        data = c.recv(1024)
-        if not data:
-            print('bye')
-            
-            #Lock released on exit
-            print_lock.release()
-            break
-        
-        #reverse the given string from client
-        data = data[::-1]
-        
-        #send back reversed string to client
-        c.send(data)
-        
-    #Connection closed
-    c.close()
-    
-def Main():
-    host = ""
-    #Reserve a port on your computer
-    #In this case is 8808
-    port = 8808
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, port))
-    print("Socket binded to port", port)
-    
-    #put the socket into listening mode
-    s.listen(5)
-    print("Socket is listening")
-    
-    #A forever loop until client wants to exit
-    while True:
-        
-        #Establish connection with client
-        c, addr = s.accept()
-        
-        #Lock acquired by client
-        print_lock.acquire()
-        print("Connected to :", addr[0] , ":", addr[1])
-    
-        #Start a new thread and return its indentifier
-        start_new_thread(threaded, (c,))
-    s.close()
-    
-if __name__ == '__main__':
-    Main()
+class ClientThread(threading.Thread):
+    def __init__(self, clientAddress, clientsocket):
+        threading.Thread.__init__(self)
+        self.csocket = clientsocket
+        print("New connection added: ", clientAddress)
+    def run(self):
+        print("Connection from : ", clientAddress)
+        #self.csocket.send(bytes("Hi, This is from Server..",'utf-8'))
+        msg = ""
+        while True:
+            data = self.csocket.recv(2048)
+            msg = data.decode()
+            spellChecker(msg)
+            if msg=='bye':
+              break
+            #print ("from client", msg)
+            self.csocket.send(bytes(msg,'UTF-8'))
+        print ("Client at ", clientAddress , " disconnected...")
+LOCALHOST = "127.0.0.1"
+PORT = 8080
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((LOCALHOST, PORT))
+print("Server started")
+print("Waiting for client request..")
+while True:
+    server.listen(1)
+    clientsock, clientAddress = server.accept()
+    newthread = ClientThread(clientAddress, clientsock)
+    newthread.start()
